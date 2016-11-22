46340f54ba1ebde7465bc0455d729b9c95302fbd
SandChow <srisandeep@hotmail.com>
2016-11-21 20:31:58 -0800

    Bugs fixed

--- insertion.py
+++ insertion.py
@@ -1,13 +1,15 @@
 def insertion_sort(array):
-    for i in range(len(array)-1):
-        if array[i+1] < array[i]:
-            tmp = array[i+1]
-            for j in xrange(i+1, 0):
-                array[j] = array[j-1]
-                print array
-            array[0] = tmp
-    return array
+    if array:
+        for i in xrange(1, len(array)):
+            if array[i] < array[i-1]:
+                tmp = array[i]
+                j = i
+                while tmp < array[j-1] and j > 0:
+                    array[j] = array[j-1]
+                    j -= 1
+                array[j] = tmp
+        return array
 
 if __name__=="__main__":
-    print insertion_sort([3, 1, 2])
+    print insertion_sort(None)
 
